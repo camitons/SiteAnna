471a5265dc29c329d6709239a1615ddddff778b3
camitons <albin.caminade@gmail.com>
2025-03-23 05:37:08 +0100

    feat : test js injection footer header

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
 // Fonction pour injecter le header
 function injectHeader() {
-    fetch('../FooterHeader/header.html')
-        .then(response => response.text())
+    fetch('/FooterHeader/header.html')  // Utilisation d'un chemin absolu
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
+        })
+        .catch(error => {
+            console.error('Erreur lors du chargement du header:', error);
         });
 }
 
 // Fonction pour injecter le footer
 function injectFooter() {
-    fetch('../FooterHeader/footer.html')
-        .then(response => response.text())
+    fetch('/FooterHeader/footer.html')  // Utilisation d'un chemin absolu
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
         .then(data => {
             document.body.insertAdjacentHTML('beforeend', data);
+        })
+        .catch(error => {
+            console.error('Erreur lors du chargement du footer:', error);
         });
 }
 
 // Appel des fonctions
-injectHeader();
-injectFooter();
+document.addEventListener('DOMContentLoaded', (event) => {
+    injectHeader();
+    injectFooter();
+});
