a0d650c3a7c3fec15135425277b1225fbfd4b11e
camitons <albin.caminade@gmail.com>
2025-03-23 06:04:45 +0100

    feat : eh bah c'est pas glorieux, le plus simple c'est de tout mettre dans le meme dossier x( franchement la honte sur moi

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Fonction pour inclure le header
 function includeHeader() {
-    fetch('/FooterHeader/header.html')
+    fetch('/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
@@ -9,7 +9,7 @@ function includeHeader() {
 
 // Fonction pour inclure le footer
 function includeFooter() {
-    fetch('/FooterHeader/footer.html')
+    fetch('/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
